dd09484aad4a9882425b53c631369fff5bf1bd0e
marianajane <mariana.musm@gmail.com>
2017-04-26 14:11:52 -0500

    Cambios

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,22 @@
+var logo = document.getElementById('logo');
+var imgBlanco = document.createElement('img');
+var imgRosa = document.createElement('img');
+
+
+logo.style.border= 4;
+imgBlanco.src = "assets/img/logo-white.png";
+imgBlanco.className = "logoBlanco";
+imgRosa.src = "assets/img/logo-pink.png";
+imgRosa.className = "logoBlanco";
+
+
+logo.appendChild(imgBlanco);
+logo.appendChild(imgRosa);
+var seccionCambio = document.getElementById('seccion2');
+seccionCambio.addEventListener('moveend',function () {
+window.scrollTo(0, 100);
+});
+
 var botonEnviarForm = document.getElementById("enviarFormulario");
 var botonSignRide = document.getElementById("signRide");
 var inputIngresarNumero = document.getElementById("ingresarNumero");
