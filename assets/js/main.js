bcab5b233ed82b3474b93bad031f16ec8863fc5b
Lucero07 <ing.lucero.ramos@gmail.com>
2017-04-27 02:18:31 -0500

    finalLu

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,22 +9,22 @@ logo.appendChild(imgBlanco);
 
 
 
-document.addEventListener("scroll", function () {
-cambioClase();
-// cambiarTexto();
-// cambiarImagen();
-// aparecerBoton();
-aparecerBoton();
+document.addEventListener("scroll", function() {
+  cambioClase();
+  // cambiarTexto();
+  // cambiarImagen();
+  // aparecerBoton();
+  aparecerBoton();
 });
 
 function cambioClase() {
   var encabezado = document.getElementById('seccion1');
-    encabezado.style.background = "white";
-    imgBlanco.src = "assets/img/logo-pink.png";
-    var menu= document.getElementsByClassName("menu-li");
-    for (var i = 0; i < menu.length; i++) {
-      menu[i].className = "menu-li textoNegro";
-    }
+  encabezado.style.background = "white";
+  imgBlanco.src = "assets/img/logo-pink.png";
+  var menu = document.getElementsByClassName("menu-li");
+  for (var i = 0; i < menu.length; i++) {
+    menu[i].className = "menu-li textoNegro";
+  }
 }
 // function cambiarImagen() {
 //
@@ -39,30 +39,29 @@ function aparecerBoton() {
 
 
 
-
 var botonEnviarForm = document.getElementById("enviarFormulario");
 var botonSignRide = document.getElementById("signRide");
 var inputIngresarNumero = document.getElementById("ingresarNumero");
 
-botonEnviarForm.addEventListener("click",mostrarFormularioCompleto);
+botonEnviarForm.addEventListener("click", mostrarFormularioCompleto);
 
-botonSignRide.addEventListener("click",esconderFormularioCompleto);
-inputIngresarNumero.addEventListener("click",mostrarFormularioCompleto);
-var divformulario= document.getElementById("imputsEscondidos");
+botonSignRide.addEventListener("click", esconderFormularioCompleto);
+inputIngresarNumero.addEventListener("click", mostrarFormularioCompleto);
+var divformulario = document.getElementById("imputsEscondidos");
 
-function mostrarFormularioCompleto(){
+function mostrarFormularioCompleto() {
   /*esta funcion debe de extender el formulario completo*/
-  divformulario.style.visibility="visible";
-  divformulario.className="extenderFormulario"; //clase a checar con estilos
+  divformulario.style.visibility = "visible";
+  divformulario.className = "extenderFormulario"; //clase a checar con estilos
 }
 
-function esconderFormularioCompleto(){
+function esconderFormularioCompleto() {
   /*esta funcion debe de esconder el formulario*/
-  divformulario.style.visibility="hidden";
-  divformulario.className="esconderFormulario" //Esconde el formulario desde el css checar con estilos
+  divformulario.style.visibility = "hidden";
+  divformulario.className = "esconderFormulario" //Esconde el formulario desde el css checar con estilos
 }
 
-function cambiarColorBotonARosa(){
+function cambiarColorBotonARosa() {
   //esta funcion es para cambiar los colores de los botones del formulario
 
 }
